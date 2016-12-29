66249b164e9e61685f5bcf365fc98affb052b637
defendthehoneypot <doug@defendthehoneypot.com>
2016-12-29 13:13:14 -0800

    updated

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-#Command line tool to collect basic information from remote systems
+#Command line tool to collect DFIR information from remote systems.
+*doug@defendthehoneypot.com
+
+I designed this to be easy to use tool to collect information from the command line.
+Version 1.0
+
+-Search for files within Users directory.
+-Collects the process list
+-Collects netstat
+-Collects DNS information
+-Collects autorun information
+-Collects streams information (uncomment if needed)
--- collect_forensics.cmd
+++ collect_forensics.cmd
@@ -10,6 +10,7 @@ REM
 REM Additional Tools Needed:
 REM Microsoft psexec.exe, Download and Info: https://technet.microsoft.com/en-us/sysinternals/pxexec
 REM Microsoft autorunssc.exe, Download and Info: https://technet.microsoft.com/en-us/sysinternals/bb963902
+REM Microsoft pslist.exe, Download and Info: https://technet.microsoft.com/en-us/sysinternals/pslist.aspx
 REM 
 REM Folder Structure:
 REM		- Main Directory containing collect_forensics.cmd
@@ -19,15 +20,8 @@ REM Setup:
 REM Set base folder, set input source file if needed, and make Results directory.
 SET scriptlocation=%~dp0
 SET src=%1
-SET dtstamp=%date:~-4%%date:~4,2%%date:~7,2%
+SET dtstamp=%date:~-4%%date:~4,2%%date:~7,2%_%time:~0,2%_%time:~3,2%_%time:~6,2%
 mkdir Results_%dtstamp%
-
-echo.
-echo #######################################
-echo Create Local Directory for tools and output
-echo #######################################
-echo.
-for /F %%i in (hosts.txt) do mkdir %scriptlocation%Results_%dtstamp%\%%i
 
 echo.
 echo #######################################
@@ -41,49 +35,54 @@ echo #######################################
 echo copy executable files to remote system
 echo #######################################
 echo.
-for /F %%i in (hosts.txt) do xcopy /y %scriptlocation%\tools\autorunsc.exe \\%%i\c$\windows\tools
+for /F %%i in (hosts.txt) do xcopy /y %scriptlocation%tools\autorunsc.exe \\%%i\c$\windows\tools
 
 echo.
 echo #######################################
 echo Search for files within Users directory
 echo #######################################
 echo.
-for /F %%i in (hosts.txt) do dir /s /a \\%%i\c$\Users\*.exe  >> %scriptlocation%Results_%dtstamp%\%%i\%%i-binaries.txt
-for /F %%i in (hosts.txt) do dir /s /a \\%%i\c$\Users\*.ps1  >> %scriptlocation%Results_%dtstamp%\%%i\%%i-binaries.txt
-for /F %%i in (hosts.txt) do dir /s /a \\%%i\c$\Users\*.vbs  >> %scriptlocation%Results_%dtstamp%\%%i\%%i-binaries.txt
-for /F %%i in (hosts.txt) do dir /s /a \\%%i\c$\Users\*.vb  >> %scriptlocation%Results_%dtstamp%\%%i\%%i-binaries.txt
+for /F %%i in (hosts.txt) do dir /s /a \\%%i\c$\Users\*.exe \\%%i\c$\Users\*.vbs \\%%i\c$\Users\*.vb \\%%i\c$\Users\*.ps1  >> %scriptlocation%Results_%dtstamp%\%%i-executablefiles.txt
+
+echo.
+echo #######################################
+echo Collect process list
+echo #######################################
+echo.
+for /F %%i in (hosts.txt) do %scriptlocation%\tools\pslist.exe -accepteula -nobanner -t %%i >> %scriptlocation%Results_%dtstamp%\%%i-processlist.csv
 
 echo.
 echo #######################################
 echo Collect network statistics command
 echo #######################################
 echo.
-for /F %%i in (hosts.txt) do psexec.exe -accepteula \\%%i netstat -n >> %scriptlocation%Results_%dtstamp%\%%i\%%i-networkinfo.txt
+for /F %%i in (hosts.txt) do %scriptlocation%\tools\psexec.exe -accepteula -nobanner \\%%i netstat -n >> %scriptlocation%Results_%dtstamp%\%%i-netstat.txt
 
 echo.
 echo #######################################
 echo Collect DNS
 echo #######################################
 echo.
-for /F %%i in (hosts.txt) do psexec.exe -accepteula \\%%i ipconfig /displaydns >> %scriptlocation%Results_%dtstamp%\%%i\%%i-dns.txt
+for /F %%i in (hosts.txt) do %scriptlocation%\tools\psexec.exe -accepteula -nobanner \\%%i ipconfig /displaydns >> %scriptlocation%Results_%dtstamp%\%%i-dns.txt
 
 echo.
 echo #######################################
 echo Run autorunsc.exe to collect information about autostart locations
 echo #######################################
 echo.
-for /F %%i in (hosts.txt) do psexec.exe -accepteula \\%%i c:\windows\tools\autorunsc.exe -accepteula -a * -ct >> %scriptlocation%Results_%dtstamp%\%%i\%%i.csv
+for /F %%i in (hosts.txt) do %scriptlocation%\tools\psexec.exe -accepteula -nobanner \\%%i c:\windows\tools\autorunsc.exe -accepteula -a * -ct >> %scriptlocation%Results_%dtstamp%\%%i-autoruns.csv
+
+REM echo.
+REM echo #######################################
+REM echo Run streams to collect ADS information
+REM echo #######################################
+REM echo.
+REM for /F %%i in (hosts.txt) do %scriptlocation%\tools\streams.exe -accepteula -nobanner -s \\%%i\c$\windows\system32 >> %scriptlocation%Results_%dtstamp%\%%i-streams.txt
 
 echo.
 echo #######################################
-echo Run streams to collect ADS information
+echo delete remote folder created on remote system
 echo #######################################
 echo.
-for /F %%i in (hosts.txt) do streams.exe -accepteula -s \\%%i\c$\windows\system32 >> %scriptlocation%Results_%dtstamp%\%%i\%%i-streams.txt
-
-echo.
-echo #######################################
-echo delete executable files copied to remote system
-echo #######################################
-echo.
-for /F %%i in (hosts.txt) do del \\%%i\c$\windows\tools\*.exe
+for /F %%i in (hosts.txt) do rmdir /s /q \\%%i\c$\windows\tools\
+echo remote folder deleted
